15896c2fbbe14496c3a0800e3b3c129bde8d462f
thomaslao <thomaslao@gmail.com>
2022-02-22 23:21:13 +0800

    sample01

---
+++ 10-array-01-sample.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a[50];  //因為n保證不超過50
+	int n;
+	cin >> n;
+	for(int i=0;i<n;i++){
+	    cin >> a[i];
+	}
+	for(int i=0;i<n;i++){
+	    cout << a[i] << "　";  //把陣列裡的每一個值印出來，以空白隔開
+	}	
+	for(int i=4;i>=0;i--){
+		cout << endl;
+		cout << "n:" <<a[i] << " i:" << i ;
+	} 
+	return 0;
+	
+	
+}
